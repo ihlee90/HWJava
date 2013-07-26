4d09b138f1dd8f4571de3b143d78ffe80a81404d
Ig-hoon Lee <ihlee90@gmail.com>
2013-07-26 20:39:18 +0900

    Chap 1 finish

---
+++ java_hw/src/java_hw/Pawn.java
@@ -0,0 +1,17 @@
+package java_hw;
+
+public class Pawn {
+	private String color ;
+	
+	Pawn( String pColor){
+		color = pColor;
+	}
+	
+	public void setColor(String pColor) {
+		color = pColor;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+}
